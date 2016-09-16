597a4408b42093e9e29829c165f6a1d2315bbcf2
liberaliscomputing <liberalis.computing@gmail.com>
2016-09-16 12:39:52 -0400

    add search function and connecting leaders

--- apps/network-mapping-js/js/manipulate.js
+++ apps/network-mapping-js/js/manipulate.js
@@ -2,15 +2,34 @@
  * Created by openhtml on 9/1/16.
  */
 // set the height of the visualisation as responsive
-//var w = window.innerWidth;
+var w = window.innerWidth;
 var h = window.innerHeight;
-$("#vis").css("height", h);
+
+$("#vis").css({ "height": h, "width": w });
 
 // initialize flip
-$("#card").flip();
-$("#card").css({ "height": h, "background": "#fff" });
+//$("#leaders").flip();
+//$(".card").css({ "height": h, "background": "#fff" });
 
+/*
 // initialize tooltip
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
 })
+
+// initialize scrollspy
+$(document).ready(function(){
+	$('body').scrollspy({target: ".navbar", offset: 50});
+	$("#navbar a").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+});
+*/
